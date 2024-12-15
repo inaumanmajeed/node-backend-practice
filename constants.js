5b905474fdeed4c85243b857e05104e67cc6e282
Nauman <inaumanmajeed@gmail.com>
2024-12-16 00:44:56 +0500

    implement Express server setup, update database name, and add utility classes for API error and response handling

--- constants.js
+++ constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME = 'ProjectOne';
+export const DB_NAME = "NodeProjectForPracticeOne";
--- db/index.js
+++ db/index.js
@@ -3,10 +3,8 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(
-      `${process.env.MONGO_URI}/${DB_NAME}`
-    );
-    console.log(`MONGODB connected: ${connect.connection.host}`);
+   const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    console.log(`\nMONGODB connected successfully on HOST: ${connectionInstance.connection.host}`);
   } catch (error) {
     console.error(`MONGODB connection Error: ${error.message}`);
     process.exit(1);
